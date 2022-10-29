108b6089652ae436bf4a3ecfb2c1694646a97022
Manideep Polireddi <okmanideep@users.noreply.github.com>
2022-10-30 04:10:31 +0530

    Add restart command

--- src/cli/index.js
+++ src/cli/index.js
@@ -4,6 +4,11 @@ import { program } from 'commander'
 import start from './start.js'
 import stop from './stop.js'
 
+async function restart() {
+  await stop()
+  await start()
+}
+
 program
   .command('start')
   .description('Start the mote server if it\'s not already started')
@@ -13,5 +18,10 @@ program
   .command('stop')
   .description('Stop the mote server if it is running')
   .action(stop)
+
+program
+  .command('restart')
+  .description('Restart the mote server')
+  .action(restart)
 
 program.parse()
