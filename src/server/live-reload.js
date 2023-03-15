98bef13043fba01303104379ef7cd89558e5a771
Manideep Polireddi <okmanideep@users.noreply.github.com>
2023-03-16 00:59:06 +0530

    Improve live update experience
    * Anchors the page at the end, if the scroll is at the end
    * Remove unnecessary updates from mote -> browser, when file is updated

--- src/server/live-reload.js
+++ src/server/live-reload.js
@@ -1,91 +1,108 @@
 import mermaid from 'mermaid'
 
 const THEME_VARIABLES = {
-  'dark': {
-      'fontFamily': '"Jetbrains Mono", monospace',
-      'background': '#22262C',
-      'mainBkg': '#282C34',
-      'primaryBorderColor': '#9AA2B1',
-      'actorBorder': '#9AA2B1',
-      'edgeLabelBackground': '#424856'
-  },
-  'light': {
-      'fontFamily': '"Jetbrains Mono", monospace',
-  }
+	'dark': {
+		'fontFamily': '"Jetbrains Mono", monospace',
+		'background': '#22262C',
+		'mainBkg': '#282C34',
+		'primaryBorderColor': '#9AA2B1',
+		'actorBorder': '#9AA2B1',
+		'edgeLabelBackground': '#424856'
+	},
+	'light': {
+		'fontFamily': '"Jetbrains Mono", monospace',
+	}
 }
 
 function _initialize(startOnLoad = true) {
-  let body = document.querySelector('body')
+	let body = document.querySelector('body')
 
-  let theme = body.getAttribute('data-theme')
-  if (!theme) {
-    theme = 'dark'
-  }
+	let theme = body.getAttribute('data-theme')
+	if (!theme) {
+		theme = 'dark'
+	}
 
-  console.log(`Initializing mermaid with ${theme} theme`)
-  mermaid.initialize(
-    {
-      startOnLoad,
-      theme,
-      themeVariables: THEME_VARIABLES[theme]
-    }
-  )
+	console.log(`Initializing mermaid with ${theme} theme`, { startOnLoad} )
+	mermaid.initialize(
+		{
+			startOnLoad,
+			theme,
+			themeVariables: THEME_VARIABLES[theme]
+		}
+	)
 }
 
 function _onContentChange() {
-  // re processes all mermaid code blocks
-  _initialize(false)
-  mermaid.init('.mermaid')
+	// re processes all mermaid code blocks
+	_initialize(false)
+	mermaid.init('.mermaid')
 }
 
 let diagrams = {
-  initialize: _initialize,
-  onContentChange: _onContentChange
+	initialize: _initialize,
+	onContentChange: _onContentChange
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  diagrams.initialize()
+	console.log("DOMContentLoaded")
+	diagrams.initialize()
 
-  const path = window.location.pathname
-  const ws = new WebSocket(`ws://localhost:{{WEBSOCKET_PORT}}${path}`)
-  ws.onopen = () => {
-    console.log("Connected")
-  }
+	const path = window.location.pathname
+	const ws = new WebSocket(`ws://localhost:{{WEBSOCKET_PORT}}${path}`)
+	ws.onopen = () => {
+		console.log("Connected")
+	}
 
-  ws.onmessage = (msg) => {
-    const payload = JSON.parse(msg.data)
+	ws.onmessage = (msg) => {
+		const payload = JSON.parse(msg.data)
 
-    if (payload.type === 'update') {
-      const contentDiv = document.querySelector('.content')
+		console.log('message', payload.type);
+		if (payload.type === 'update') {
+			const contentDiv = document.querySelector('.content')
 
-      // record scrollTop to restore after diagrams are processed
-      let scrollTop = document.documentElement.scrollTop
+			// record scrollTop to restore after diagrams are processed
+			let scrollTop = document.documentElement.scrollTop
+			let scrollHeight = document.documentElement.scrollHeight
+			const windowHeight = window.innerHeight
+			let isAtEnd = false
+			if (scrollTop + windowHeight === scrollHeight) {
+				isAtEnd = true
+			}
 
-      contentDiv.innerHTML = payload.data.contents
-      diagrams.onContentChange()
+			contentDiv.innerHTML = payload.data.contents
+			diagrams.onContentChange()
 
-      // restore the scroll position of the document after content changes
-      // normally we don't have to do this, but since the mermaid diagrams
-      // are processed and swapped(code -> svg) on the browser, there is
-      // that occurs after scrolling past one or more diagrams
-      document.documentElement.scrollTop = scrollTop
-    } else if (payload.type === 'error') {
-      console.error(payload.data.message)
-    }
-  }
+			// restore the scroll position of the document after content changes
+			// normally we don't have to do this, but since the mermaid diagrams
+			// are processed and swapped(code -> svg) on the browser, there is
+			// that occurs after scrolling past one or more diagrams
+			if (isAtEnd) {
+				// stay at the end of scroll
+				// for this we need to wait for the diagrams to be rendered
+				setTimeout(() => {
+					document.documentElement.scrollTop = document.documentElement.scrollHeight - windowHeight
+				})
+			} else {
+				// stay at the previous scroll top
+				document.documentElement.scrollTop = scrollTop
+			}
+		} else if (payload.type === 'error') {
+			console.error(payload.data.message)
+		}
+	}
 
-  /*
-  * Refetching markdown content to re-render mermaid diagrams in the new theme
-  * Hacky, but works 🤷
-  */
-  function onThemeChange() {
-    ws.send(JSON.stringify({type: 'theme-changed'}))
-  }
+	/*
+* Refetching markdown content to re-render mermaid diagrams in the new theme
+* Hacky, but works 🤷
+*/
+	function onThemeChange() {
+		ws.send(JSON.stringify({type: 'theme-changed'}))
+	}
 
-  let body = document.querySelector("body") 
-  let themeChangeObserver = new MutationObserver(onThemeChange)
+	let body = document.querySelector("body") 
+	let themeChangeObserver = new MutationObserver(onThemeChange)
 
-  themeChangeObserver.observe(body, {
-    attributeFilter: ['data-theme']
-  })
+	themeChangeObserver.observe(body, {
+		attributeFilter: ['data-theme']
+	})
 })
--- src/server/server.js
+++ src/server/server.js
@@ -5,89 +5,93 @@ import fs from 'fs'
 import conf from './conf.js'
 import mdRenderer from './md-renderer.js'
 import PageRenderer from './page-renderer-webc.js'
+import CryptoJS from 'crypto-js'
 
 async function start() {
-  const config = await conf.get()
-  const SITE_PORT = config['site_port']
-  const MOTES_DIR = config['motes_dir']
-  const WEBSOCKET_PORT = config['websocket_port']
-  const pageRenderer = new PageRenderer(WEBSOCKET_PORT)
+	const config = await conf.get()
+	const SITE_PORT = config['site_port']
+	const MOTES_DIR = config['motes_dir']
+	const WEBSOCKET_PORT = config['websocket_port']
+	const pageRenderer = new PageRenderer(WEBSOCKET_PORT)
 
-  const mdFilePath = (filename) => {
-    return path.join(MOTES_DIR, `${filename}.md`)
-  }
+	const mdFilePath = (filename) => {
+		return path.join(MOTES_DIR, `${filename}.md`)
+	}
 
-  const app = express()
+	const app = express()
 
-  app.get('/status', async function(_, res) {
-      res.sendStatus(200)
-  })
+	app.get('/status', async function(_, res) {
+		res.sendStatus(200)
+	})
 
-  app.get('/favicon.ico', async function(_, res) {
-      res.sendStatus(404)
-  })
+	app.get('/favicon.ico', async function(_, res) {
+		res.sendStatus(404)
+	})
 
-  app.get('/p/:path', async function(req, res) {
-    const filePath = req.params['path']
-    if (!fs.existsSync(filePath)) {
-      res.status(404).send(`<h1>${filePath} not found</h1>`)
-    } else {
-      res.send(await pageRenderer.render(filePath))
-    }
-  })
+	app.get('/p/:path', async function(req, res) {
+		const filePath = req.params['path']
+		if (!fs.existsSync(filePath)) {
+			res.status(404).send(`<h1>${filePath} not found</h1>`)
+		} else {
+			res.send(await pageRenderer.render(filePath))
+		}
+	})
 
-  app.get('/:name', async function(req, res) {
-    const name = req.params['name']
-    const file = mdFilePath(name)
+	app.get('/:name', async function(req, res) {
+		const name = req.params['name']
+		const file = mdFilePath(name)
 
-    if (!fs.existsSync(file)) {
-      console.log(`Could not find ${file}`)
-      res.sendStatus(404)
-    } else {
-      res.send(await pageRenderer.render(file))
-    }
-  })
+		if (!fs.existsSync(file)) {
+			console.log(`Could not find ${file}`)
+			res.sendStatus(404)
+		} else {
+			res.send(await pageRenderer.render(file))
+		}
+	})
 
-  app.listen(SITE_PORT, () => {
-    console.log(`http://localhost:${SITE_PORT}/current`)
-  })
+	app.listen(SITE_PORT, () => {
+		console.log(`http://localhost:${SITE_PORT}/current`)
+	})
 
-  const wss = new WebSocketServer({ port: WEBSOCKET_PORT })
-  wss.on('connection', (ws, req) => {
-    let file
+	const wss = new WebSocketServer({ port: WEBSOCKET_PORT })
+	wss.on('connection', (ws, req) => {
+		let file
 
-    if (req.url.startsWith('/p/')) {
-      file = decodeURIComponent(req.url.substring(3))
-    } else {
-      const filename = req.url.substring(1)
-      file = mdFilePath(filename)
-    }
+		if (req.url.startsWith('/p/')) {
+			file = decodeURIComponent(req.url.substring(3))
+		} else {
+			const filename = req.url.substring(1)
+			file = mdFilePath(filename)
+		}
 
-    if (!fs.existsSync(file)) {
-      ws.send(JSON.stringify({ type: 'error', data: { message: `Couldn't find ${file}` } }))
-      return
-    }
+		if (!fs.existsSync(file)) {
+			ws.send(JSON.stringify({ type: 'error', data: { message: `Couldn't find ${file}` } }))
+			return
+		}
 
-    fs.watchFile(file, { persistent: true, interval: 300 }, async () => {
-      console.log("file updated")
-      const contents = mdRenderer.render(file)
-      ws.send(JSON.stringify({ type: 'update', data: { contents } }))
-    })
+		let prevContentHash = ''
+		fs.watchFile(file, { persistent: true, interval: 300 }, async () => {
+			const contents = mdRenderer.render(file)
+			const newContentHash = CryptoJS.MD5(contents).toString()
+			if (newContentHash !== prevContentHash) {
+				ws.send(JSON.stringify({ type: 'update', data: { contents } }))
+				prevContentHash = newContentHash
+			}
+		})
 
-    ws.onclose = () => {
-      fs.unwatchFile(file)
-    }
+		ws.onclose = () => {
+			fs.unwatchFile(file)
+		}
 
-    ws.onmessage = (msg) => {
-      const payload = JSON.parse(msg.data)
+		ws.onmessage = (msg) => {
+			const payload = JSON.parse(msg.data)
 
-      if (payload.type === 'theme-changed') {
-        console.log("theme changed")
-        const contents = mdRenderer.render(file) 
-        ws.send(JSON.stringify({ type: 'update', data: { contents } }))
-      }
-    }
-  })
+			if (payload.type === 'theme-changed') {
+				const contents = mdRenderer.render(file) 
+				ws.send(JSON.stringify({ type: 'update', data: { contents } }))
+			}
+		}
+	})
 
 }
 
