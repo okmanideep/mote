c054a6317c65ecb2f8e2bbe37a4147dde5b3a20d
Manideep Polireddi <okmanideep@users.noreply.github.com>
2024-08-12 16:42:36 +0530

    try/catch on file updates and reformat server.js

--- src/server/server.js
+++ src/server/server.js
@@ -11,88 +11,92 @@ const __filename = fileURLToPath(import.meta.url); // get the resolved path to t
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 async function start() {
-	const config = await conf.get()
-	const SITE_PORT = config['site_port']
-	const MOTES_DIR = config['motes_dir']
-	const WEBSOCKET_PORT = config['websocket_port']
-	const pageRenderer = new PageRenderer(WEBSOCKET_PORT)
+  const config = await conf.get()
+  const SITE_PORT = config['site_port']
+  const MOTES_DIR = config['motes_dir']
+  const WEBSOCKET_PORT = config['websocket_port']
+  const pageRenderer = new PageRenderer(WEBSOCKET_PORT)
 
-	const mdFilePath = (filename) => {
-		return path.join(MOTES_DIR, `${filename}.md`)
-	}
+  const mdFilePath = (filename) => {
+    return path.join(MOTES_DIR, `${filename}.md`)
+  }
 
-	const app = express()
+  const app = express()
 
-	app.get('/status', async function(_, res) {
-		res.sendStatus(200)
-	})
+  app.get('/status', async function(_, res) {
+    res.sendStatus(200)
+  })
 
-	app.use(express.static(path.join(__dirname,'assets')))
+  app.use(express.static(path.join(__dirname,'assets')))
 
-	app.get('/p/:path', async function(req, res) {
-		const filePath = req.params['path']
-		if (!fs.existsSync(filePath)) {
-			res.status(404).send(`<h1>${filePath} not found</h1>`)
-		} else {
-			res.send(await pageRenderer.render(filePath))
-		}
-	})
+  app.get('/p/:path', async function(req, res) {
+    const filePath = req.params['path']
+    if (!fs.existsSync(filePath)) {
+      res.status(404).send(`<h1>${filePath} not found</h1>`)
+    } else {
+      res.send(await pageRenderer.render(filePath))
+    }
+  })
 
-	app.get('/:name', async function(req, res) {
-		const name = req.params['name']
-		const file = mdFilePath(name)
+  app.get('/:name', async function(req, res) {
+    const name = req.params['name']
+    const file = mdFilePath(name)
 
-		if (!fs.existsSync(file)) {
-			console.log(`Could not find ${file}`)
-			res.sendStatus(404)
-		} else {
-			res.send(await pageRenderer.render(file))
-		}
-	})
+    if (!fs.existsSync(file)) {
+      console.log(`Could not find ${file}`)
+      res.sendStatus(404)
+    } else {
+      res.send(await pageRenderer.render(file))
+    }
+  })
 
-	app.listen(SITE_PORT, () => {
-		console.log(`http://localhost:${SITE_PORT}/current`)
-	})
+  app.listen(SITE_PORT, () => {
+    console.log(`http://localhost:${SITE_PORT}/current`)
+  })
 
-	const wss = new WebSocketServer({ port: WEBSOCKET_PORT })
-	wss.on('connection', (ws, req) => {
-		let file
+  const wss = new WebSocketServer({ port: WEBSOCKET_PORT })
+  wss.on('connection', (ws, req) => {
+    let file
 
-		if (req.url.startsWith('/p/')) {
-			file = decodeURIComponent(req.url.substring(3))
-		} else {
-			const filename = req.url.substring(1)
-			file = mdFilePath(filename)
-		}
+    if (req.url.startsWith('/p/')) {
+      file = decodeURIComponent(req.url.substring(3))
+    } else {
+      const filename = req.url.substring(1)
+      file = mdFilePath(filename)
+    }
 
-		if (!fs.existsSync(file)) {
-			ws.send(JSON.stringify({ type: 'error', data: { message: `Couldn't find ${file}` } }))
-			return
-		}
+    if (!fs.existsSync(file)) {
+      ws.send(JSON.stringify({ type: 'error', data: { message: `Couldn't find ${file}` } }))
+      return
+    }
 
-		let prevContentHash = ''
-		fs.watchFile(file, { persistent: true, interval: 300 }, async () => {
-			const contents = mdRenderer.render(file)
-			const newContentHash = CryptoJS.MD5(contents).toString()
-			if (newContentHash !== prevContentHash) {
-				ws.send(JSON.stringify({ type: 'update', data: { contents } }))
-				prevContentHash = newContentHash
-			}
-		})
+    let prevContentHash = ''
+    fs.watchFile(file, { persistent: true, interval: 300 }, async () => {
+      try {
+        const contents = mdRenderer.render(file)
+        const newContentHash = CryptoJS.MD5(contents).toString()
+        if (newContentHash !== prevContentHash) {
+          ws.send(JSON.stringify({ type: 'update', data: { contents } }))
+          prevContentHash = newContentHash
+        }
+      } catch (e) {
+        console.error(`Error watching file: ${file}`, e)
+      }
+    })
 
-		ws.onclose = () => {
-			fs.unwatchFile(file)
-		}
+    ws.onclose = () => {
+      fs.unwatchFile(file)
+    }
 
-		ws.onmessage = (msg) => {
-			const payload = JSON.parse(msg.data)
+    ws.onmessage = (msg) => {
+      const payload = JSON.parse(msg.data)
 
-			if (payload.type === 'theme-changed') {
-				const contents = mdRenderer.render(file) 
-				ws.send(JSON.stringify({ type: 'update', data: { contents } }))
-			}
-		}
-	})
+      if (payload.type === 'theme-changed') {
+        const contents = mdRenderer.render(file) 
+        ws.send(JSON.stringify({ type: 'update', data: { contents } }))
+      }
+    }
+  })
 
 }
 
